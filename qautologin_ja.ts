4ff44a2b93a0d5ba1812614282d8767d73e2dba8
futr <ilce.ma@gmail.com>
2014-03-15 03:42:47 +0900

    翻訳を更新

--- qautologin_ja.ts
+++ qautologin_ja.ts
@@ -5,7 +5,7 @@
     <name>MainWidget</name>
     <message>
         <location filename="mainwidget.ui" line="14"/>
-        <location filename="mainwidget.cpp" line="347"/>
+        <location filename="mainwidget.cpp" line="375"/>
         <source>QAutoLogin</source>
         <translation></translation>
     </message>
@@ -46,147 +46,162 @@
     </message>
     <message>
         <location filename="mainwidget.ui" line="144"/>
+        <source>Rand&amp;omize! (min)</source>
+        <translation>自動再ログイン間隔を揺らす(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="mainwidget.ui" line="171"/>
         <source>Next login to %v[min]</source>
         <translation>次のログインまであと%v分</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="158"/>
+        <location filename="mainwidget.ui" line="185"/>
         <source>&amp;Stop timer</source>
         <translation>タイマー停止(&amp;S)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="178"/>
-        <location filename="mainwidget.cpp" line="358"/>
+        <location filename="mainwidget.ui" line="205"/>
+        <location filename="mainwidget.cpp" line="386"/>
         <source>Log&amp;out</source>
         <translation>ログアウト(&amp;O)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="185"/>
-        <location filename="mainwidget.cpp" line="357"/>
+        <location filename="mainwidget.ui" line="212"/>
+        <location filename="mainwidget.cpp" line="385"/>
         <source>&amp;Login</source>
         <translation>ログイン(&amp;L)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="227"/>
+        <location filename="mainwidget.ui" line="258"/>
         <source>Re&amp;new IP</source>
         <translation>IPを再取得(&amp;n)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="234"/>
+        <location filename="mainwidget.ui" line="265"/>
         <source>Check &amp;IP</source>
         <translation>IP確認(&amp;I)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="259"/>
+        <location filename="mainwidget.ui" line="290"/>
         <source>Enable save/load &amp;user id</source>
         <translation>ユーザーIDの保存・読み込み有効(&amp;U)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="266"/>
+        <location filename="mainwidget.ui" line="297"/>
         <source>Enable save/load &amp;password</source>
         <translation>パスワードの保存・読み込み有効(&amp;P)</translation>
     </message>
     <message>
+        <location filename="mainwidget.ui" line="304"/>
+        <source>Logout &amp;when application closes</source>
+        <translation>終了時にログアウトする(&amp;W)</translation>
+    </message>
+    <message>
         <location filename="mainwidget.ui" line="311"/>
+        <source>Renew IP a&amp;fter Login/Logout ( Windows only )</source>
+        <translation>ログイン・ログアウト時にIPを再取得する( Windowsのみ )(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="mainwidget.ui" line="356"/>
         <source>&amp;Quit</source>
         <translation>終了(&amp;Q)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="304"/>
+        <location filename="mainwidget.ui" line="349"/>
         <source>Minimize to &amp;tray</source>
         <translation>トレイへ最小化(&amp;T)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="206"/>
+        <location filename="mainwidget.ui" line="233"/>
         <source>Message</source>
         <translation>メッセージ</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="245"/>
+        <location filename="mainwidget.ui" line="276"/>
         <source>&amp;Minimize to systemtray when application starts</source>
         <translation>起動時にトレイへ最小化(&amp;M)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="252"/>
+        <location filename="mainwidget.ui" line="283"/>
         <source>Login with saved id and password when application &amp;starts</source>
         <translation>起動時に保存されたIDとパスワードでログインする(&amp;S)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="284"/>
+        <location filename="mainwidget.ui" line="329"/>
         <source>&amp;About Qt</source>
         <translation>About Qt(&amp;A)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="277"/>
+        <location filename="mainwidget.ui" line="322"/>
         <source>Save &amp;Config</source>
         <translation>設定を保存(&amp;C)</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="117"/>
+        <location filename="mainwidget.cpp" line="140"/>
         <source>Login</source>
         <translation>ログイン</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="118"/>
+        <location filename="mainwidget.cpp" line="141"/>
         <source>Login time	 : </source>
         <translation>ログイン時間	 : </translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="119"/>
+        <location filename="mainwidget.cpp" line="142"/>
         <source>
 Logout time	 : </source>
         <translation>
 ログアウト時間	 : </translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="126"/>
+        <location filename="mainwidget.cpp" line="149"/>
         <source>Logout</source>
         <translation>ログアウト</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="134"/>
+        <location filename="mainwidget.cpp" line="157"/>
         <source>Error</source>
         <translation>エラー</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="127"/>
+        <location filename="mainwidget.cpp" line="150"/>
         <source>Logout succeeded</source>
         <translation>ログアウト完了</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="135"/>
+        <location filename="mainwidget.cpp" line="158"/>
         <source>Some errors occurred</source>
         <translation>いくつかの問題が発生しました</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="254"/>
+        <location filename="mainwidget.cpp" line="291"/>
         <source>Ssl warning</source>
         <translation>SSL利用に関する警告</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="254"/>
+        <location filename="mainwidget.cpp" line="291"/>
         <source>
 
 These &quot;SslError&quot; are ignored.</source>
         <translation>これらのSSLエラーは無視されます。</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="278"/>
+        <location filename="mainwidget.cpp" line="315"/>
         <source>Network error</source>
         <translation>ネットワークエラー</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="278"/>
+        <location filename="mainwidget.cpp" line="315"/>
         <source>Some network error occurred</source>
         <translation>いくつかのネットワークエラーが発生しました</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="356"/>
+        <location filename="mainwidget.cpp" line="384"/>
         <source>&amp;Close</source>
         <translation>終了(&amp;C)</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="359"/>
+        <location filename="mainwidget.cpp" line="387"/>
         <source>&amp;Show</source>
         <translation>ウィンドウを表示(&amp;S)</translation>
     </message>
