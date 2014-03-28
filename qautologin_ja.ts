d49d7dc2efd94a5c77b8f51e85d029ec457f4a32
futr <ilce.ma@gmail.com>
2014-03-29 01:30:27 +0900

    翻訳を更新

--- qautologin_ja.ts
+++ qautologin_ja.ts
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ja_JP">
+<TS version="2.1" language="ja_JP">
 <context>
     <name>MainWidget</name>
     <message>
         <location filename="mainwidget.ui" line="14"/>
-        <location filename="mainwidget.cpp" line="375"/>
+        <location filename="mainwidget.cpp" line="405"/>
         <source>QAutoLogin</source>
         <translation></translation>
     </message>
@@ -61,13 +61,13 @@
     </message>
     <message>
         <location filename="mainwidget.ui" line="205"/>
-        <location filename="mainwidget.cpp" line="386"/>
+        <location filename="mainwidget.cpp" line="416"/>
         <source>Log&amp;out</source>
         <translation>ログアウト(&amp;O)</translation>
     </message>
     <message>
         <location filename="mainwidget.ui" line="212"/>
-        <location filename="mainwidget.cpp" line="385"/>
+        <location filename="mainwidget.cpp" line="415"/>
         <source>&amp;Login</source>
         <translation>ログイン(&amp;L)</translation>
     </message>
@@ -98,16 +98,22 @@
     </message>
     <message>
         <location filename="mainwidget.ui" line="311"/>
-        <source>Renew IP a&amp;fter Login/Logout ( Windows only )</source>
-        <translation>ログイン・ログアウト時にIPを再取得する( Windowsのみ )(&amp;F)</translation>
+        <source>Renew IP a&amp;fter first Login ( Windows only )</source>
+        <oldsource>Renew IP a&amp;fter Login/Logout ( Windows only )</oldsource>
+        <translation>最初のログイン時にIPを再取得する( Windowsのみ )(&amp;F)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="356"/>
+        <location filename="mainwidget.ui" line="318"/>
+        <source>Renew IP after Lo&amp;gout ( Windows only )</source>
+        <translation>ログアウト時にIPを再取得する( Windowsのみ )(&amp;G)</translation>
+    </message>
+    <message>
+        <location filename="mainwidget.ui" line="363"/>
         <source>&amp;Quit</source>
         <translation>終了(&amp;Q)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="349"/>
+        <location filename="mainwidget.ui" line="356"/>
         <source>Minimize to &amp;tray</source>
         <translation>トレイへ最小化(&amp;T)</translation>
     </message>
@@ -127,81 +133,91 @@
         <translation>起動時に保存されたIDとパスワードでログインする(&amp;S)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="329"/>
+        <location filename="mainwidget.ui" line="336"/>
         <source>&amp;About Qt</source>
         <translation>About Qt(&amp;A)</translation>
     </message>
     <message>
-        <location filename="mainwidget.ui" line="322"/>
+        <location filename="mainwidget.ui" line="329"/>
         <source>Save &amp;Config</source>
         <translation>設定を保存(&amp;C)</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="140"/>
+        <location filename="mainwidget.cpp" line="148"/>
         <source>Login</source>
         <translation>ログイン</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="141"/>
+        <location filename="mainwidget.cpp" line="149"/>
         <source>Login time	 : </source>
         <translation>ログイン時間	 : </translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="142"/>
+        <location filename="mainwidget.cpp" line="150"/>
         <source>
 Logout time	 : </source>
         <translation>
 ログアウト時間	 : </translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="149"/>
+        <location filename="mainwidget.cpp" line="157"/>
         <source>Logout</source>
         <translation>ログアウト</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="157"/>
+        <location filename="mainwidget.cpp" line="165"/>
         <source>Error</source>
         <translation>エラー</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="150"/>
+        <location filename="mainwidget.cpp" line="158"/>
         <source>Logout succeeded</source>
         <translation>ログアウト完了</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="158"/>
+        <location filename="mainwidget.cpp" line="166"/>
         <source>Some errors occurred</source>
         <translation>いくつかの問題が発生しました</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="291"/>
+        <location filename="mainwidget.cpp" line="311"/>
         <source>Ssl warning</source>
         <translation>SSL利用に関する警告</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="291"/>
+        <location filename="mainwidget.cpp" line="311"/>
         <source>
 
 These &quot;SslError&quot; are ignored.</source>
         <translation>これらのSSLエラーは無視されます。</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="315"/>
+        <location filename="mainwidget.cpp" line="335"/>
         <source>Network error</source>
         <translation>ネットワークエラー</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="315"/>
+        <location filename="mainwidget.cpp" line="335"/>
         <source>Some network error occurred</source>
         <translation>いくつかのネットワークエラーが発生しました</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="384"/>
+        <location filename="mainwidget.cpp" line="353"/>
+        <source>Updating IP address</source>
+        <translation>IPアドレス更新中</translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="362"/>
+        <source>Update complete</source>
+        <translation>更新完了</translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="414"/>
         <source>&amp;Close</source>
         <translation>終了(&amp;C)</translation>
     </message>
     <message>
-        <location filename="mainwidget.cpp" line="387"/>
+        <location filename="mainwidget.cpp" line="417"/>
         <source>&amp;Show</source>
         <translation>ウィンドウを表示(&amp;S)</translation>
     </message>
